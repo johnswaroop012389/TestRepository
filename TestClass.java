464128ba5826255eace798a10fc8e27ce5bdcd77
johnswaroop012389 <john.swaroop@smnetserv.com>
2025-05-21 14:38:21 +0530

    Add files via upload

---
+++ TestClass.java
@@ -0,0 +1,11 @@
+package com.test.projectAPI;
+
+public class TestClass {
+
+	public static String runTest() {
+		System.out.println("Executing automated test case...");
+		String expected = "pass";
+		String actual = "pass";
+		return expected.equals(actual) ? "Test Passed!" : "Test Failed!";
+	}
+}
