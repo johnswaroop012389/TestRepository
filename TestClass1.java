ff6e4bb1ae7fa8675bd0bc13ac7ff779314336fa
johnswaroop012389 <john.swaroop@smnetserv.com>
2025-05-29 12:47:35 +0530

    Update TestClass1.java

--- TestClass1.java
+++ TestClass1.java
@@ -1,15 +1,15 @@
-public class TestClass {
+public class TestClass1 {
 
     public static void main(String[] args) {
-        System.out.println("Executing automated test case...");
+        System.out.println("Executing automated test case1...");
         
         String expected = "pass";
         String actual = "pass";
 
         if (expected.equals(actual)) {
-            System.out.println("Test Passed!");
+            System.out.println("Test1 Passed!");
         } else {
-            System.out.println("Test Failed!");
+            System.out.println("Test1 Failed!");
         }
     }
 }
