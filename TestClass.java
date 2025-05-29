d68f7de68994142e8e9fd738b78b0847d8a85460
johnswaroop012389 <john.swaroop@smnetserv.com>
2025-05-29 14:39:12 +0530

    Update TestClass.java

--- TestClass.java
+++ TestClass.java
@@ -3,21 +3,30 @@ import java.io.PrintWriter;
 
 public class TestClass {
     public static void main(String[] args) throws Exception {
-        String runTarget = args.length > 0 ? args[0] : "both";
+        String runTarget = args.length > 0 ? args[0].toLowerCase() : "both";
 
-        // Overwrite result.txt with header
         PrintWriter writer = new PrintWriter("result.txt");
         writer.println("== Java Test Execution Results ==");
         writer.println("Run Target: " + runTarget);
         writer.close();
 
-        if ("class1".equalsIgnoreCase(runTarget)) {
-            new TestClass1().run();
-        } else if ("class2".equalsIgnoreCase(runTarget)) {
-            new TestClass2().run();
-        } else {
-            new TestClass1().run();
-            new TestClass2().run();
+        switch (runTarget) {
+            case "class1":
+                new TestClass1().run();
+                break;
+            case "class2":
+                new TestClass2().run();
+                break;
+            case "both":
+                new TestClass1().run();
+                new TestClass2().run();
+                break;
+            default:
+                PrintWriter errorWriter = new PrintWriter(new FileWriter("result.txt", true));
+                errorWriter.println("❌ Invalid input: " + runTarget);
+                errorWriter.println("Please use one of: class1, class2, both");
+                errorWriter.close();
+                break;
         }
     }
 }
