89566a1284f9da7c5e1aba623c3f3638ae4cb259
johnswaroop012389 <john.swaroop@smnetserv.com>
2025-06-04 19:43:54 +0530

    Add files via upload

---
+++ JavaProjectExample/src/com/TestPackage/TestClass1.java
@@ -0,0 +1,12 @@
+package com.TestPackage;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+public class TestClass1 {
+    public void run() throws Exception {
+        PrintWriter writer = new PrintWriter(new FileWriter("result.txt", true));
+        writer.println(">> Running TestClass1...");
+        writer.println("Result: TestClass1 Passed ✅");
+        writer.close();
+    }
+}
