9ebffa5335b0c295363b5933f2e8eaef4fb22b94
johnswaroop012389 <john.swaroop@smnetserv.com>
2025-06-05 21:41:58 +0530

    Update TC02.java

--- src/test/java/com/TestPackage/TC02.java
+++ src/test/java/com/TestPackage/TC02.java
@@ -4,10 +4,7 @@ import java.io.PrintWriter;
 
 public class TC02 {
     public void run() throws Exception {
-        PrintWriter writer = new PrintWriter(new FileWriter("results.txt", true));
-        writer.println(">> Running TestClass2...");
-        writer.println("Result: TestClass2 Passed ✅");
-        writer.close();
+
         
     }
 }
