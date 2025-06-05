c420a4063dbf3ddd0b38a89cf0f9526c737a8eb6
johnswaroop012389 <john.swaroop@smnetserv.com>
2025-06-05 19:42:09 +0530

    Update BaseTest.java

--- src/test/java/com/TestPackage/BaseTest.java
+++ src/test/java/com/TestPackage/BaseTest.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.*;
 
+import org.openqa.selenium.chrome.ChromeOptions; // add this import
+
 public class BaseTest {
 	public static WebDriver driver;
 	public static PrintWriter writer;
@@ -17,6 +19,13 @@ public class BaseTest {
     public void setup() throws FileNotFoundException {
 		writer = new PrintWriter("result.txt");
         io.github.bonigarcia.wdm.WebDriverManager.chromedriver().setup();
+
+	ChromeOptions options = new ChromeOptions()     
+        options.addArguments("--user-data-dir=/tmp/chrome-user-data-" + System.currentTimeMillis());
+        options.addArguments("--headless=new"); 
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+	    
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://uat.qaconnector.com/Fintech/tenantLogin");
