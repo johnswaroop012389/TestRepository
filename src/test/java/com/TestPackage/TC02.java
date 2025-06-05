c706155c0484917fc76f3d9aebb6ae7308b36a01
johnswaroop012389 <john.swaroop@smnetserv.com>
2025-06-05 21:50:54 +0530

    Update TC02.java

--- src/test/java/com/TestPackage/TC02.java
+++ src/test/java/com/TestPackage/TC02.java
@@ -3,6 +3,11 @@ import java.io.FileWriter;
 import java.io.PrintWriter;
 
 public class TC02 {
+
+    public TC02(PrintWriter writer) {
+        this.writer = writer;
+    }
+    
     public void run() throws Exception {
 
         
