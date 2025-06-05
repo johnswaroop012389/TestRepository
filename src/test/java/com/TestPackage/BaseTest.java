aa6e9cdd13eb2bbf0a365e0c17a1fb8ae66db225
johnswaroop012389 <john.swaroop@smnetserv.com>
2025-06-05 18:07:57 +0530

    Add files via upload

---
+++ src/test/java/com/TestPackage/BaseTest.java
@@ -0,0 +1,66 @@
+package com.TestPackage;
+
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+public class BaseTest {
+	public static WebDriver driver;
+	public static PrintWriter writer;
+	public static String runTarget;
+	
+	@BeforeClass
+    public void setup() throws FileNotFoundException {
+		writer = new PrintWriter("result.txt");
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://uat.qaconnector.com/Fintech/tenantLogin");
+        writer.print("Test Execution Started ");
+        writer.println("Launched"+ " https://uat.qaconnector.com/Fintech/tenantLogin "+ "Successufully");
+    }
+
+    @AfterClass
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+            writer.println("Browser closed");
+            writer.print("Test Execution Ended ");
+        }
+    }
+    
+    @Test
+    public void triggerTests() {
+    	try {
+    		runTarget = System.getProperty("runTarget", "both").toLowerCase(); 
+			writer.println("== Java Test Execution Results ==");
+			writer.println("Running " + runTarget);
+
+			switch (runTarget) {
+			case "class1":
+				new TC01().run();
+				break;
+			case "class2":
+				new TC02().run();
+				break;
+			case "both":
+				new TC01().run();
+				new TC02().run();
+				break;
+			default:
+				PrintWriter errorWriter = new PrintWriter(new FileWriter("result.txt", true));
+				errorWriter.println("❌ Invalid input: " + runTarget);
+				errorWriter.println("Please use one of: class1, class2, both");
+				errorWriter.close();
+				break;
+			}
+		} catch (Exception e) {
+			writer.println(e);
+		} 
+    }
+	
+}
---
+++ src/test/java/com/TestPackage/TC01.java
@@ -0,0 +1,12 @@
+package com.TestPackage;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
+public class TC01 {
+    public void run() throws Exception {
+        PrintWriter writer = new PrintWriter(new FileWriter("result.txt", true));
+        writer.println(">> Running TestClass1...");
+        writer.println("Result: TestClass1 Passed ✅");
+        writer.close();
+    }
+}
