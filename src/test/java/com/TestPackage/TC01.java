f0baf04d023c6552464cf1374602010e5bfab638
johnswaroop012389 <john.swaroop@smnetserv.com>
2025-06-06 10:58:40 +0530

    Update TC01.java

--- src/test/java/com/TestPackage/TC01.java
+++ src/test/java/com/TestPackage/TC01.java
@@ -1,6 +1,10 @@
 package com.TestPackage;
 import java.io.PrintWriter;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
 public class TC01 {
 
     private PrintWriter writer;
@@ -9,9 +13,15 @@ public class TC01 {
         this.writer = writer;
     }
     
-    public void run() throws Exception {
+    public void run(WebDriver driver) throws Exception {
         writer.println("🚀 TC01: Test started.");
-        
+        Assert.assertTrue(driver.findElements(By.xpath("//span[contains(text(), 'Dashboard')]")).size() > 0, "Expected element not found!");
+        Assert.assertTrue(driver.findElements(By.xpath("//span[contains(text(), 'Projects')]")).size() > 0, "Expected element not found!");
+        Assert.assertTrue(driver.findElements(By.xpath("//span[contains(text(), 'Test Bank')]")).size() > 0, "Expected element not found!");
+        Assert.assertTrue(driver.findElements(By.xpath("//span[contains(text(), 'Test Plans')]")).size() > 0, "Expected element not found!");
+        Assert.assertTrue(driver.findElements(By.xpath("//span[contains(text(), 'Test Execution')]")).size() > 0, "Expected element not found!");
+        Assert.assertTrue(driver.findElements(By.xpath("//span[contains(text(), 'Defects')]")).size() > 0, "Expected element not found!");
+        Assert.assertTrue(driver.findElements(By.xpath("//span[contains(text(), 'Admin')]")).size() > 0, "Expected element not found!");       
         writer.println("✅ TC01: Test completed.");
     }
 }
