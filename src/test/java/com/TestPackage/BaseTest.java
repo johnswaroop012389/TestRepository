4fb1ab53c56690b5ef9bd909382a7c42a3fb6c6b
johnswaroop012389 <john.swaroop@smnetserv.com>
2025-06-06 10:43:20 +0530

    Update BaseTest.java

--- src/test/java/com/TestPackage/BaseTest.java
+++ src/test/java/com/TestPackage/BaseTest.java
@@ -11,56 +11,54 @@ import org.testng.annotations.*;
 
 import org.testng.Assert;
 
-import java.io.IOException;
-
 public class BaseTest {
 	public static WebDriver driver;
 	public static PrintWriter writer;
 	public static String runTarget;
-	
+
 	@BeforeClass
-    public void setup() throws IOException  {
-	writer = new PrintWriter("results.txt");
-	    
-	io.github.bonigarcia.wdm.WebDriverManager.chromedriver().setup();
-	   
-	ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless=new");  // modern headless mode
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--remote-allow-origins=*");
+	public void setup() throws Exception {
+		writer = new PrintWriter("results.txt");
 
-        driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
-        driver.get("https://uat.qaconnector.com/Fintech/tenantLogin");
-        writer.println("Launched"+ " https://uat.qaconnector.com/Fintech/tenantLogin "+ "Successufully");
+		io.github.bonigarcia.wdm.WebDriverManager.chromedriver().setup();
 
-	driver.findElement(By.name("user_name")).sendKeys("amith.nadig");
-	writer.println("Username Entered Successufully");
-	driver.findElement(By.name("password")).sendKeys("ViratKohli@123");
-	writer.println("Password Entered Successufully");
-	driver.findElement(By.id("Login_loginbtn__7Tj03")).click();
-	writer.println("Clicked on Login Successufully");
-	String actualText = driver.findElement(By.className("landingPage_companyName__2RiNM")).getText();
-	Assert.assertTrue(actualText.contains("Fintech Solutions"));
-	writer.println("Homepage Loaded Successufully");
-    }
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--headless=new"); // modern headless mode
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--disable-gpu");
+		options.addArguments("--remote-allow-origins=*");
 
-    @AfterClass
-    public void teardown() {
-        if (driver != null) {
-            driver.quit();
-            writer.println("Browser closed");
-	    writer.flush();  // Add this to flush buffer
-            writer.close();  // Add this to close the writer properly
-        }
-    }
-    
-    @Test
-    public void triggerTests() {
-    	try {
-    		runTarget = System.getProperty("runTarget", "all").toLowerCase(); 
+		driver = new ChromeDriver(options);
+		driver.manage().window().maximize();
+		driver.get("https://uat.qaconnector.com/Fintech/tenantLogin");
+		writer.println("Launched" + " https://uat.qaconnector.com/Fintech/tenantLogin " + "Successufully");
+
+		driver.findElement(By.name("user_name")).sendKeys("amith.nadig");
+		writer.println("Username Entered Successufully");
+		driver.findElement(By.name("password")).sendKeys("ViratKohli@123");
+		writer.println("Password Entered Successufully");
+		driver.findElement(By.id("Login_loginbtn__7Tj03")).click();
+		writer.println("Clicked on Login Successufully");
+		Thread.sleep(2000);
+		Assert.assertTrue(driver.findElements(By.className("landingPage_companyName__2RiNM")).size() > 0, "Element not found!");
+		writer.println("Homepage Loaded Successufully");
+	}
+
+	@AfterClass
+	public void teardown() {
+		if (driver != null) {
+			driver.quit();
+			writer.println("Browser closed");
+			writer.flush(); // Add this to flush buffer
+			writer.close(); // Add this to close the writer properly
+		}
+	}
+
+	@Test
+	public void triggerTests() {
+		try {
+			runTarget = System.getProperty("runTarget", "all").toLowerCase();
 
 			switch (runTarget) {
 			case "test1":
@@ -82,7 +80,7 @@ public class BaseTest {
 			}
 		} catch (Exception e) {
 			writer.println(e);
-		} 
-    }
-	
+		}
+	}
+
 }
