7f402e6427f52dac0a2bc4284b3288253c719148
johnswaroop012389 <john.swaroop@smnetserv.com>
2025-06-11 20:16:18 +0530

    Update TestLogger.java

--- src/test/java/com/TestPackage/TestLogger.java
+++ src/test/java/com/TestPackage/TestLogger.java
@@ -5,18 +5,23 @@ import java.io.PrintWriter;
 public class TestLogger {
 	private static PrintWriter writer;
 
+	// ANSI escape codes for colors
+	private static final String RESET = "\u001B[0m";
+	private static final String GREEN = "\u001B[32m";
+	private static final String RED = "\u001B[31m";
+
 	public static void setWriter(PrintWriter w) {
 		writer = w;
 	}
 
 	public static void logPass(String message) {
 		if (writer != null)
-			writer.println("[PASS] " + message);
+			writer.println(GREEN + "[PASS] " + message + RESET);
 	}
 
 	public static void logFail(String message, Exception e) {
 		if (writer != null)
-			writer.println("[FAIL] " + message + " - " + e.getMessage());
+			writer.println(RED + "[FAIL] " + message + " - " + e.getMessage() + RESET);
 	}
 
 	public static void logStep(String message, Runnable step) {
