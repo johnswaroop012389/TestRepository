e90bbf984bb8df8557cb7168f8f2e34e681bfa8d
johnswaroop012389 <john.swaroop@smnetserv.com>
2025-06-11 12:34:34 +0530

    Add files via upload

---
+++ src/test/java/com/TestPackage/TestLogger.java
@@ -0,0 +1,30 @@
+package com.TestPackage;
+
+import java.io.PrintWriter;
+
+public class TestLogger {
+	private static PrintWriter writer;
+
+	public static void setWriter(PrintWriter w) {
+		writer = w;
+	}
+
+	public static void logPass(String message) {
+		if (writer != null)
+			writer.println("[PASS] " + message);
+	}
+
+	public static void logFail(String message, Exception e) {
+		if (writer != null)
+			writer.println("[FAIL] " + message + " - " + e.getMessage());
+	}
+
+	public static void logStep(String message, Runnable step) {
+		try {
+			step.run(); // Run the test step
+			logPass(message);
+		} catch (Exception e) {
+			logFail(message, e);
+		}
+	}
+}
