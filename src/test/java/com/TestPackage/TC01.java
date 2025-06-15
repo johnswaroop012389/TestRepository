e8a6f8ebc12cf4b98a1558192f9c5000814398be
johnswaroop012389 <john.swaroop@smnetserv.com>
2025-06-15 15:00:23 +0530

    Update TC01.java

--- src/test/java/com/TestPackage/TC01.java
+++ src/test/java/com/TestPackage/TC01.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 @Test
 public class TC01 {
