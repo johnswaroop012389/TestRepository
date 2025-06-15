6cd481c149f889a84c3230c7eaaae6329e3455ce
johnswaroop012389 <john.swaroop@smnetserv.com>
2025-06-15 15:00:37 +0530

    Update TC02.java

--- src/test/java/com/TestPackage/TC02.java
+++ src/test/java/com/TestPackage/TC02.java
@@ -7,7 +7,9 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
+@Test
 public class TC02 {
 
 	public void run(WebDriver driver) throws Exception {
