8bb1398634a1c3d4902cc41e44bd15d37f929bba
johnswaroop012389 <john.swaroop@smnetserv.com>
2025-06-15 15:50:15 +0530

    Update BaseTest.java

--- src/test/java/com/TestPackage/BaseTest.java
+++ src/test/java/com/TestPackage/BaseTest.java
@@ -1,7 +1,6 @@
 package com.TestPackage;
 
 import java.io.PrintWriter;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -9,99 +8,60 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.*;
 
 import org.testng.Assert;
-import org.testng.SkipException;
 
-@Listeners(ConditionalTestFilter.class)
 public class BaseTest {
-	public static WebDriver driver;
-	public static PrintWriter writer;
-	public static String runTarget;
+    public static WebDriver driver;
+    public static PrintWriter writer;
 
-	@BeforeClass
-	public void setup() throws Exception {
-		writer = new PrintWriter("results.txt");
-		TestLogger.setWriter(writer); // register writer
+    @BeforeClass
+    public void setup() throws Exception {
+        writer = new PrintWriter("results.txt");
+        TestLogger.setWriter(writer); // register writer
 
-		io.github.bonigarcia.wdm.WebDriverManager.chromedriver().setup();
+        io.github.bonigarcia.wdm.WebDriverManager.chromedriver().setup();
 
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--headless=new"); // modern headless mode
-		options.addArguments("--no-sandbox");
-		options.addArguments("--disable-dev-shm-usage");
-		options.addArguments("--disable-gpu");
-		options.addArguments("--remote-allow-origins=*");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--disable-gpu");
+        options.addArguments("--remote-allow-origins=*");
 
-		driver = new ChromeDriver(options);
-		driver.manage().window().maximize();
+        driver = new ChromeDriver(options);
+        driver.manage().window().maximize();
 
-		// Launch Url
-		TestLogger.logStep("Launched" + " https://uat.qaconnector.com/Fintech/tenantLogin " + "Successufully", () -> {
-			driver.get("https://uat.qaconnector.com/Fintech/tenantLogin");
-		});
+        TestLogger.logStep("Launched https://uat.qaconnector.com/Fintech/tenantLogin Successfully", () -> {
+            driver.get("https://uat.qaconnector.com/Fintech/tenantLogin");
+        });
 
-		// Enter username
-		TestLogger.logStep("Username Entered Successfully", () -> {
-			driver.findElement(By.name("user_name")).sendKeys("amith.nadig");
-		});
+        TestLogger.logStep("Username Entered Successfully", () -> {
+            driver.findElement(By.name("user_name")).sendKeys("amith.nadig");
+        });
 
-		// Enter password
-		TestLogger.logStep("Password Entered Successfully", () -> {
-			driver.findElement(By.name("password")).sendKeys("ViratKohli@123");
-		});
+        TestLogger.logStep("Password Entered Successfully", () -> {
+            driver.findElement(By.name("password")).sendKeys("ViratKohli@123");
+        });
 
-		// Click on login button
-		TestLogger.logStep("Clicked on Login Successfully", () -> {
-			driver.findElement(By.id("Login_loginbtn__7Tj03")).click();
-		});
-		Thread.sleep(2000);
+        TestLogger.logStep("Clicked on Login Successfully", () -> {
+            driver.findElement(By.id("Login_loginbtn__7Tj03")).click();
+        });
 
-		// Page loaded successfully
-		TestLogger.logStep("Homepage Loaded Successfully", () -> {
-			Assert.assertTrue(driver.findElements(By.className("landingPage_companyName__2RiNM")).size() > 0,
-					"Element not found!");
-		});
-	}
+        Thread.sleep(2000);
 
-	@AfterClass
-	public void teardown() {
-		if (driver != null) {
-			TestLogger.logStep("Browser closed", () -> {
-				driver.quit();
-			});
-			writer.flush(); // Add this to flush buffer
-			writer.close(); // Add this to close the writer properly
-		}
-	}
+        TestLogger.logStep("Homepage Loaded Successfully", () -> {
+            Assert.assertTrue(driver.findElements(By.className("landingPage_companyName__2RiNM")).size() > 0,
+                    "Element not found!");
+        });
+    }
 
-	@Test
-	public void runTC01() {
-	    if (!shouldRun("test1") && !shouldRun("all")) {
-	        throw new SkipException("Skipping TC01 based on runTarget");
-	    }
-	    writer.println("<!-- 🧪 Starting Test Case: TC01 -->");
-	    try {
-	        new TC01().run(driver);
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    }
-	}
-
-	@Test
-	public void runTC02() {
-	    if (!shouldRun("test2") && !shouldRun("all")) {
-	        throw new SkipException("Skipping TC02 based on runTarget");
-	    }
-	    writer.println("<!-- 🧪 Starting Test Case: TC02 -->");
-	    try {
-	        new TC02().run(driver);
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    }
-	}
-
-	private boolean shouldRun(String target) {
-		String runTarget = System.getProperty("runTarget", "all").toLowerCase();
-		return runTarget.equals(target) || runTarget.equals("all");
-	}
-
+    @AfterClass
+    public void teardown() {
+        if (driver != null) {
+            TestLogger.logStep("Browser closed", () -> {
+                driver.quit();
+            });
+            writer.flush();
+            writer.close();
+        }
+    }
 }
